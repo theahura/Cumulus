5bace7950bc19f739a28691293c434826d584a12
Amol Kapoor <theahura@gmail.com>
2015-06-10 00:30:56 -0400

    Save function works for gdrive.
    Signed-off-by: Amol Kapoor <theahura@gmail.com>

--- Sandbox/Drive/Save/save.js
+++ Sandbox/Drive/Save/save.js
@@ -1,16 +1,88 @@
+/*
+  @author: Amol Kapoor
+  @date: 6-9-15
+  @version: 1.0
+
+  Description: File to handle all save related functions for google drive. 
+
+*/
+
+
+//---------------------------------------Load local file-----------------------------------------------
+
+//Copy pasted from interwobs. Could probably be better.
 function handleFileSelect(evt)
 {
     var files = evt.target.files; // FileList object
 
     // files is a FileList of File objects. List some properties.
     var output = [];
-    for (var i = 0, f; f = files[i]; i++) {
+    for (var i = 0, f; f = files[i]; i++) 
+    {
       output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
                   f.size, ' bytes, last modified: ',
                   f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
                   '</li>');
+
+      //Calls the save function for gdrive
+      insertFile(f)
     }
     document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
 }
 
-document.getElementById('files').addEventListener('change', handleFileSelect, false);
+document.getElementById('SaveButton').addEventListener('change', handleFileSelect, false);
+
+//------------------------------------------------------------------------------------------------------
+
+//---------------------------------------The save function-----------------------------------------------
+
+
+/**
+ * Insert new file.
+ *
+ * @param {File} fileData File object to read data from.
+ * @param {Function} callback Function to call when the request is complete.
+ */
+function insertFile(fileData, callback) {
+  const boundary = '-------314159265358979323846';
+  const delimiter = "\r\n--" + boundary + "\r\n";
+  const close_delim = "\r\n--" + boundary + "--";
+
+  var reader = new FileReader();
+  console.log(fileData)
+  reader.readAsBinaryString(fileData);
+  reader.onload = function(e) {
+    var contentType = fileData.type || 'application/octet-stream';
+    var metadata = {
+      'title': fileData.name,
+      'mimeType': contentType
+    };
+
+    var base64Data = btoa(reader.result);
+    var multipartRequestBody =
+        delimiter +
+        'Content-Type: application/json\r\n\r\n' +
+        JSON.stringify(metadata) +
+        delimiter +
+        'Content-Type: ' + contentType + '\r\n' +
+        'Content-Transfer-Encoding: base64\r\n' +
+        '\r\n' +
+        base64Data +
+        close_delim;
+
+    var request = gapi.client.request({
+        'path': '/upload/drive/v2/files',
+        'method': 'POST',
+        'params': {'uploadType': 'multipart'},
+        'headers': {
+          'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'
+        },
+        'body': multipartRequestBody});
+    if (!callback) {
+      callback = function(file) {
+        console.log(file)
+      };
+    }
+    request.execute(callback);
+  }
+}
