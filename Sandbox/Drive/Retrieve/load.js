fc26c859ce029906008238eb49cbc9ba32ad1aee
Amol Kapoor <theahura@gmail.com>
2015-06-28 00:58:09 -0400

    Drive retrieval sandboxed version works
    Signed-off-by: Amol Kapoor <theahura@gmail.com>

--- Sandbox/Drive/Retrieve/load.js
+++ Sandbox/Drive/Retrieve/load.js
@@ -1,3 +1,35 @@
+$("#RetrieveButton").click(function()
+{
+  retrieveAllFiles(function(result)
+  {
+    for(i in result)
+    {
+      if(result[i].downloadUrl)
+      {
+        document.location = result[i].webContentLink
+        return
+      }
+    }
+  });
+});
+
+/**
+ * Retrieve a list of File resources.
+ *
+ * @param {Function} callback Function to call when the request is complete.
+ */
+function retrieveAllFiles(callback) {
+  var retrievePageOfFiles = function(request, result) {
+    request.execute(function(resp) {
+      result = result.concat(resp.items);
+      var nextPageToken = resp.nextPageToken;
+      callback(result);
+    });
+  }
+  var initialRequest = gapi.client.drive.files.list();
+  retrievePageOfFiles(initialRequest, []);
+}
+
 /**
  * Print a file's metadata.
  *
@@ -21,6 +53,7 @@ function printFile(fileId) {
  * @param {Function} callback Function to call when the request is complete.
  */
 function downloadFile(file, callback) {
+  printFile(file.id)
   if (file.downloadUrl) {
     var accessToken = gapi.auth.getToken().access_token;
     var xhr = new XMLHttpRequest();
